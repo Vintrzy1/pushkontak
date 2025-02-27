a9cdd08fc555c0c7dd97c1bf7dc61515c473bd3d
Vintrzy1 <rorrrell7@gmail.com>
2025-02-27 09:49:11 +0700

    Add files via upload

---
+++ database/kosong.js
@@ -0,0 +1,1 @@
+"Buy Script Bot ? Chat 6283869453279"
---
+++ database/teksjpm.js
@@ -0,0 +1,28 @@
+"https://whatsapp.com/channel/0029Vb2wozWCBtxDUzwAmr0x"*
+> *📮 3GB = 2K/BULAN*
+> *📮 4GB = 3K/BULAN*
+> *📮 5GB = 4K/BULAN*
+> *📮 6GB = 5K/BULAN*
+> *📮 7GB = 6K/BULAN*
+> *📮 8GB = 7K/BULAN*
+> *📮 9GB = 8K/BULAN*
+> *📮 UNLI = 9K/BULAN*
+
+> *BOT PUSHKONTAK 3K*
+> *BOT BUG 5K*
+
+> *BELI PANEL UNLI FREE SC PUSHKONTAK*
+> *FREE PASANG SC*
+> *RESELLER PANEL = 10K*
+
+*MINAT PM* 6283153419618
+
+*⊹₊⋆ Benefit panel*
+> ~ 𝘙𝘶𝘯 𝘣𝘰𝘵 24 𝘫𝘢𝘮
+> ~ 𝘚𝘦𝘳𝘷𝘦𝘳 𝘱𝘳𝘪𝘷𝘢𝘵 𝘢𝘯𝘵𝘪 𝘭𝘢𝘨
+> ~ 𝘚𝘤 𝘢𝘯𝘵𝘪 𝘤𝘰𝘭𝘰𝘯𝘨
+> ~ 𝘗𝘶𝘴𝘩𝘬𝘰𝘯 𝘭𝘢𝘯𝘤𝘢𝘳
+> ~ 𝘑𝘢𝘨𝘢 𝘨𝘳𝘶𝘣 𝘭𝘢𝘯𝘤𝘢𝘳
+
+
+*TESTI* "https://whatsapp.com/channel/0029Vb2wozWCBtxDUzwAmr0x"
